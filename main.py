20c57f81445f26ac9d31af4082bed97a553d0b9b
jaesung <jaesung741@naver.com>
2020-11-20 19:37:08 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#! python3
+import requests, sys, webbrowser, bs4
+
+print('Googling...')
+res= requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
+
+soup=bs4.BeautifulSoup(res.text,features="html.parser")
+
+linkElems = soup.select('.r a')
+numOpen = min(5,len(linkElems))
+for i in range (numOpen):
+    webbrowser.open('http://google.com' + linkElems[i].get('href'))
